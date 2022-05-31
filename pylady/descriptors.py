77f6aa7fb941969b26c0f6b0252b7640fa46fa01
Arnaud Allera <arnaud.allera@gmail.com>
2022-05-31 15:07:37 +0200

    add descriptors classmethods

--- pylady/descriptors.py
+++ pylady/descriptors.py
@@ -25,3 +25,51 @@ class Descriptor():
         m = Model(database=db, descriptor=self, ml_type=-1)
         m.run()
         return m.database.collections["descriptor_only"].descriptors
+
+    @classmethod
+    def G2(cls, r_cut):
+        return cls(desc_type=1, r_cut=r_cut)
+
+    @classmethod
+    def G3(cls, r_cut):
+        return cls(desc_type=2, r_cut=r_cut)
+
+    @classmethod
+    def Behler(cls, r_cut):
+        return cls(desc_type=3, r_cut=r_cut)
+
+    @classmethod
+    def AFS(cls, r_cut):
+        return cls(desc_type=4, r_cut=r_cut)
+    
+    @classmethod
+    def SOAP(cls, r_cut):
+        return cls(desc_type=5, r_cut=r_cut)
+
+    @classmethod
+    def PSO3(cls, r_cut):
+        return cls(desc_type=6, r_cut=r_cut)
+
+    @classmethod
+    def BSO3(cls, r_cut):
+        return cls(desc_type=7, r_cut=r_cut)
+
+    @classmethod
+    def PSO4(cls, r_cut):
+        return cls(desc_type=8, r_cut=r_cut)
+    
+    @classmethod
+    def BSO4(cls, r_cut):
+        return cls(desc_type=9, r_cut=r_cut)
+
+    @classmethod
+    def Hybrid_G2_AFS(cls, r_cut):
+        return cls(desc_type=14, r_cut=r_cut)
+
+    @classmethod
+    def Hybrid_G2_BSO4(cls, r_cut):
+        return cls(desc_type=19, r_cut=r_cut)
+    
+    @classmethod
+    def MTP3(cls, r_cut):
+        return cls(desc_type=100, r_cut=r_cut)
--- pylady/tests/test_descriptor.py
+++ pylady/tests/test_descriptor.py
@@ -1,7 +1,58 @@
 import pylady
 import pytest
 
+systems = ["pylady/tests/valid.poscar"]
+args_values = [int(2), 2.5]
 
-def test_descriptor_compute():
-    d = pylady.Descriptor(desc_type=1, r_cut=2)
-    d.compute(["pylady/tests/valid.poscar"])
+@pytest.mark.parametrize("r", args_values)
+def test_descriptor_compute(r):
+    d = pylady.Descriptor(desc_type=1, r_cut=r)
+    d.compute(systems)
+
+@pytest.mark.parametrize("r", args_values)
+def test_compute_G2(r):
+    pylady.Descriptor.G2(r_cut=r).compute(systems)
+
+@pytest.mark.parametrize("r", args_values)
+def test_compute_G3(r):
+    pylady.Descriptor.G3(r_cut=r).compute(systems)
+
+@pytest.mark.parametrize("r", args_values)
+def test_compute_Behler(r):
+    pylady.Descriptor.Behler(r_cut=r).compute(systems)
+
+@pytest.mark.parametrize("r", args_values)
+def test_compute_AFS(r):
+    pylady.Descriptor.AFS(r_cut=r).compute(systems)
+
+@pytest.mark.parametrize("r", args_values)
+def test_compute_SOAP(r):
+    pylady.Descriptor.SOAP(r_cut=r).compute(systems)
+
+@pytest.mark.parametrize("r", args_values)
+def test_compute_PSO3(r):
+    pylady.Descriptor.PSO3(r_cut=r).compute(systems)
+
+@pytest.mark.parametrize("r", args_values)
+def test_compute_BSO3(r):
+    pylady.Descriptor.BSO3(r_cut=r).compute(systems)
+
+@pytest.mark.parametrize("r", args_values)
+def test_compute_PSO4(r):
+    pylady.Descriptor.PSO4(r_cut=r).compute(systems)
+
+@pytest.mark.parametrize("r", args_values)
+def test_compute_BSO4(r):
+    pylady.Descriptor.BSO4(r_cut=r).compute(systems)
+
+@pytest.mark.parametrize("r", args_values)
+def test_compute_Hybrid_G2_AFS(r):
+    pylady.Descriptor.Hybrid_G2_AFS(r_cut=r).compute(systems)
+
+@pytest.mark.parametrize("r", args_values)
+def test_compute_Hybrid_G2_BSO4(r):
+    pylady.Descriptor.Hybrid_G2_BSO4(r_cut=r).compute(systems)
+
+@pytest.mark.parametrize("r", args_values)
+def test_compute_MTP3(r):
+    pylady.Descriptor.MTP3(r_cut=r).compute(systems)
